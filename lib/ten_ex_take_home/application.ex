d4c5b3c012cdb416cf018c449620b016bbdb7bdd
Rick Oliveira <riksongleison471@gmail.com>
2023-07-12 19:56:58 -0300

    add: socket+opts to redis

--- lib/ten_ex_take_home/application.ex
+++ lib/ten_ex_take_home/application.ex
@@ -21,7 +21,7 @@ defmodule TenExTakeHome.Application do
       # Start the Endpoint (http/https)
       TenExTakeHomeWeb.Endpoint,
       # Starts redis server
-      {Redix, {redis_url, [name: :redis_server]}}
+      {Redix, {redis_url, [name: :redis_server, socket_opts: [:inet6]]}}
       # Start a worker by calling: TenExTakeHome.Worker.start_link(arg)
       # {TenExTakeHome.Worker, arg}
     ]
